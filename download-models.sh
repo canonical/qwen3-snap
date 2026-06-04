194fdd0162a269834cb8965fa1da056d6a024ea6
Ivano Matrisciano <ivano.matrisciano@canonical.com>
2026-06-04 12:04:52 +0200

    Add qwen3 4b

--- download-models.sh
+++ download-models.sh
@@ -4,3 +4,7 @@
 # 8B model
 wget -nv https://huggingface.co/unsloth/Qwen3-8B-GGUF/resolve/main/Qwen3-8B-Q4_K_M.gguf \
     --directory-prefix=components/model-8b-q4-k-m-gguf/
+
+# 4B model    
+wget -nv https://huggingface.co/unsloth/Qwen3-4B-GGUF/resolve/main/Qwen3-4B-Q4_K_M.gguf \
+    --directory-prefix=components/model-4b-q4-k-m-gguf/
